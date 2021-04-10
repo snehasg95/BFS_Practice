ed6e3b301f07743e50279f40c6e058f7264515aa
SnehaPriya <31264991+snehasg95@users.noreply.github.com>
2021-04-10 11:40:18 -0700

    Create shortest_path_to_get_food.py

---
+++ shortest_path_to_get_food.py
@@ -0,0 +1,47 @@
+class Solution:
+    def getFood(self, grid: List[List[str]]) -> int:
+        
+        if not grid or len(grid) == 0:
+            return -1
+        
+        rows = len(grid)
+        cols = len(grid[0])
+        
+        queue = collections.deque()
+        
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == '*':
+                    queue.append((i,j))
+                    
+        
+        distance = 1
+        dirs = [(0,1), (0,-1), (1,0), (-1,0)]
+        while queue:
+            
+            
+            size = len(queue)
+            for i in range(size):
+                
+                cr, cc = queue.popleft()
+                
+                for dir in dirs:
+                    r = cr + dir[0]
+                    c = cc + dir[1]
+                    
+                    if r >= 0 and r < rows and c >= 0 and c < cols and grid[r][c] != 'X':
+                        
+                        if grid[r][c] == '#':
+                            return distance
+                        
+                        if grid[r][c] == 'O':
+                            grid[r][c] = distance
+                            queue.append((r,c))
+                            
+            distance += 1
+                            
+                
+        return -1
+                
+                
+        
