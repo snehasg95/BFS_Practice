b1e0365832cf2898eb2fc30a8d6f7afcd403247c
SnehaPriya <31264991+snehasg95@users.noreply.github.com>
2021-04-10 09:41:43 -0700

    Update shortest_path_binary_matrix.py

--- shortest_path_binary_matrix.py
+++ shortest_path_binary_matrix.py
@@ -98,5 +98,59 @@ class Solution:
                         
 
             
+## With size variable 
 
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        
+        if not grid:
+            return 0
+        
+        queue = deque()
+        
+        rows = len(grid)
+        cols = len(grid[0])
+        
+        dest_row = rows - 1
+        dest_col = cols - 1
+
+        # base case
+        if grid[0][0] != 0 or grid[dest_row][dest_col] != 0:
+            return -1
+        
+        grid[0][0] = 1 # mark visited
+        queue.append((0,0)) 
+        moves = 1
+
+        # 8 dirs
+        dirs = [(0,1), (0,-1), (1,0), (-1,0), (1,1), (1,-1), (-1, 1), (-1,-1)]
+        
+        
+        while queue:
+        
+            for _ in range(len(queue)):
+                
+                cr, cc = queue.popleft()
+                # distance = grid[cr][cc]
+
+                # is it the destination already?
+                if cr == dest_row and cc == dest_col:
+                    return moves
+
+                for dir in dirs:
+                    nr = cr + dir[0]
+                    nc = cc + dir[1]
+
+                    if nr >= 0 and nr < rows and nc >= 0 and nc < cols and grid[nr][nc] == 0:
+                        # mark visited in terms of distance
+                        grid[nr][nc] = 1
+                        queue.append((nr,nc))
+                        
+            moves += 1
+
+            
+        return -1
+                        
+
+            
             
